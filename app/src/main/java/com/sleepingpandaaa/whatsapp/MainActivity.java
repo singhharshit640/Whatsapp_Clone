096beb3b511c22547508dec2301334f1954beac1
Harshit <singhharshit640@gmail.com>
2020-07-12 23:24:57 +0530

    Initial Commit

--- app/src/main/java/com/sleepingpandaaa/whatsapp/MainActivity.java
+++ app/src/main/java/com/sleepingpandaaa/whatsapp/MainActivity.java
@@ -121,7 +121,61 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.main_find_friends_option) {
 
         }
+        if (item.getItemId() == R.id.main_create_group_option)
+        {
+            RequestNewGroup();
+        }
         return true;
+    }
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("E.G. Bhailog");
+        builder.setView(groupNameField);
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            String groupName = groupNameField.getText().toString();
+            if (TextUtils.isEmpty(groupName))
+            {
+                Toast.makeText(MainActivity.this, "Please enter a group name", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                CreateNewGroup(groupName);
+            }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void CreateNewGroup(final String groupName)
+    {
+
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, groupName + " is Created Successfully!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
     }
 
 
